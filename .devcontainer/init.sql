b037b716f5904a7774b92d1a807311b558e2ec79
jaidan <jaidanseto@gmail.com>
2025-04-26 13:39:54 -0400

    add utilities table

--- .devcontainer/init.sql
+++ .devcontainer/init.sql
@@ -1,30 +1,12 @@
-DROP DATABASE IF EXISTS "TodoDB";
-CREATE DATABASE "TodoDB";
+DROP DATABASE IF EXISTS "WaterWaster";
+CREATE DATABASE "WaterWaster";
 
-\c TodoDB;
-
-DROP TYPE IF EXISTS todo_status;
-CREATE TYPE todo_status AS ENUM ('incomplete', 'complete');
-
-DROP TABLE IF EXISTS todos;
-CREATE TABLE todos (
-  id SERIAL PRIMARY KEY,
-  title VARCHAR(255) NOT NULL,
-  description TEXT NOT NULL,
-  status todo_status NOT NULL DEFAULT 'incomplete',
-  due_at TIMESTAMP,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  completed_at TIMESTAMP,
-  edited_at TIMESTAMP
-);
-
-DROP TABLE IF EXISTS subtodos;
-CREATE TABLE subtodos (
-  id SERIAL PRIMARY KEY,
-  title VARCHAR(255) NOT NULL,
-  status todo_status NOT NULL DEFAULT 'incomplete',
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  completed_at TIMESTAMP,
-  edited_at TIMESTAMP,
-  todo_id INTEGER REFERENCES todos(id) ON DELETE CASCADE
-);
+DROP TABLE IF EXISTS utilities;
+CREATE TABLE utilities (
+  utilityId SERIAL,
+  name TEXT,
+  cost numeric,
+  waterCost numeric,
+  --roomId numeric,
+  description TEXT 
+)
